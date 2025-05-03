a6b5ed51e077f6b42f6c6db829555d5711cf6bd4
KL0CKERS09 <fredobendal0808@gmail.com>
2025-05-02 23:54:57 -0800

    Update changes 123

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,10 @@
-import SecondSecClient from "../home-components/secondSecServer";
 import FirstSecClient from "../home-components/firstSecClient";
 import AnnouncementClient from "../home-components/AnnouncementSection";
-import style from "@/styles/variable.module.scss"
+import style from "./styling.module.scss"
 import FAQ from "../home-components/FAQS";
 import About from "../home-components/about";
 import MissionVision from "../home-components/MissionVision";
+import ActiveAlertsSection from "../(user)/User-Main/user-home/user-alert/page";
 
 
 export default function Home() {
@@ -13,7 +13,9 @@ export default function Home() {
     <main className="relative">
       <FirstSecClient/>
       <div className={`${style[`hr`]}`}></div>
-      <SecondSecClient/>
+      <div className="w-[80%] mx-auto">
+        <ActiveAlertsSection/>
+      </div>
       <div className={`${style[`hr`]}`}></div>
       <AnnouncementClient />
       <div className={`${style[`hr`]}`}></div>
--- app/home-components/navbar.tsx
+++ app/home-components/navbar.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "@/styles/variable.module.scss";
-import { Roboto } from 'next/font/google';
+import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -10,51 +9,50 @@ const roboto = Roboto({
 });
 
 export default function Navbar() {
-    return(
-        <>
-            <header className={`${styles["main-header"]} w-[100%] min-h-16 sticky top-0`}>
-                <div className={` ${styles.navbarGrid} w-[100%] m-auto py-5 px-4  `}>
-                    <div className="flex items-center gap-3">
-                        <Image 
-                            className=""
-                            src='/safe-net-logo.svg' 
-                            alt="SafeNet Logo" 
-                            height={70} 
-                            width={70}>
-                        </Image>
-                        <Link 
-                            className="flex flex-col" 
-                            href={"/"}>
-                                <span className={`${styles.logoTitle} ${roboto.variable} font-bold text-3xl`}>DisPERS</span> 
-                                <span className={`${styles.logoSubTitle}`}>BARANGGAY BAGONG SILANGAN ALERT SYSTEM</span>
-                        </Link>
-                    </div>
-                    <div className="min-h-16 w-[100%]">
-                        <ul className="flex justify-end items-center gap-2 w-[100%] h-[100%]">
-                            <li className={`${styles.li}`}>
-                                <Link 
-                                className={`${styles.Link} text-xl px-7 py-2 text-gray-900  font-semibold hover:bg-orange-100 `} 
-                                href={"/"}>Home</Link>
-                            </li>
-                            <li className={`${styles.li}`}>
-                                <Link 
-                                className={`${styles.Link} text-xl px-7 py-2 text-gray-900  font-semibold hover:bg-orange-100 `}
-                                href={"../About"}>About</Link>
-                            </li>
-                            <li className={`${styles.li}`}>
-                                <Link 
-                                className={`${styles.Link} text-xl px-7 py-2 text-gray-900  font-semibold hover:bg-orange-100 `}
-                                href={"../contact"}>Contact</Link>
-                            </li>
-                            <li>
-                                <Link 
-                                className={`${styles.logInBtn} px-15 py-4 rounded-4xl text-xl text-white   bg-[#4E709D] hover:bg-blue-500 hover:text-gray-200`}
-                                href={"../Login"}>Log In</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </header>
-        </>
-    );
+  return (
+    <header className="w-full min-h-16 sticky top-0 bg-[#ffd2aeca] z-[999]">
+      <div className="w-full m-auto py-5 px-4 grid [grid-template-columns:repeat(auto-fit,minmax(500px,1fr))] scale-[0.75]">
+        <div className="flex items-center gap-3">
+          <Image 
+            src="/safe-net-logo.svg"
+            alt="SafeNet Logo"
+            height={70}
+            width={70}
+          />
+          <Link href="/" className="flex flex-col">
+            <span className={`${roboto.variable} font-bold text-3xl tracking-[2px] text-[#FF5632] relative`}>
+              DisPERS
+            </span>
+            <span className="text-[#333] font-medium">
+              BARANGGAY BAGONG SILANGAN ALERT SYSTEM
+            </span>
+          </Link>
+        </div>
+
+        <div className="min-h-16 w-full">
+          <ul className="flex justify-end items-center gap-2 w-full h-full">
+            {["Home", "About", "Contact"].map((label, index) => (
+              <li key={label} className="relative group">
+                <span className="absolute left-0 bottom-[-10px] h-[5px] w-0 bg-[#FF5632] transition-all duration-400 ease-in-out group-hover:w-full"></span>
+                <Link
+                  href={index === 0 ? "/" : `../${label.toLowerCase()}`}
+                  className="text-xl px-7 py-2 text-gray-900 font-semibold hover:bg-orange-100 group-hover:text-[#d85700]"
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
+            <li>
+              <Link
+                href="../Login"
+                className="italic font-bold px-13 py-4 rounded-2xl text-xl text-white bg-[#4E709D] hover:bg-blue-500 hover:text-gray-200"
+              >
+                Log In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 }
